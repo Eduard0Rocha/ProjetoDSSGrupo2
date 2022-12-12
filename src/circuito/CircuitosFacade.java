f188a73275da14cde95355d42a749848793ff84c
Bernardo Escudeiro <73354104+jbtescudeiro16@users.noreply.github.com>
2022-12-12 20:12:57 +0000

    Add files via upload

---
+++ src/circuito/CircuitosFacade.java
@@ -0,0 +1,50 @@
+package circuito;
+
+import carro.C1;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import circuito.Exceptions.*;
+
+public class CircuitosFacade implements SGestCircuitos {
+
+    private HashMap <String,Circuito> Circuitos;
+    private  int circuitosCounter;
+
+    public CircuitosFacade(){
+        this.Circuitos=new HashMap<>();
+        this.circuitosCounter=0;
+    }
+
+    public boolean  createCicruito(int voltas, String nomeCirc, String local, float dist, List<Integer> curvas, List<Integer> retas, List<Integer> chicanes, float temperatura, int humidade, float temp_Asf, String estado_climaterico,int drs){
+        String codCirc = new String("Circuito" + Integer.toString(this.circuitosCounter));
+        this.circuitosCounter++;
+        if (this.Circuitos.containsKey(codCirc)){
+            Circuito c= new Circuito(codCirc,voltas,nomeCirc,local,dist,drs);
+            c.setCondicoesATM(new CondicoesAtmosfericas(temp_Asf,temperatura,humidade,estado_climaterico));
+            this.Circuitos.put(codCirc  ,c.clone());
+            return true;
+        }
+        return false;
+    }
+    public boolean changeCondicoesATM(String codCirc, float temperatura, int humidade, float temp_Asf, String estado_climaterico){
+        if (this.Circuitos.containsKey(codCirc)){
+            this.Circuitos.get(codCirc).setCondicoesATM(new CondicoesAtmosfericas(temp_Asf,temperatura,humidade,estado_climaterico));
+            return true;
+        }
+        return false;
+      }
+      public  Circuito getCircuito(String codCirc) throws NonExistantKey{
+          if (Circuitos.containsKey(codCirc)){
+              return this.Circuitos.get(codCirc).clone();
+          }
+          else throw new NonExistantKey("Chave inexistente");
+      }
+      public Map<String,Circuito> getCircuitos(){
+        Map<String,Circuito> copy = new HashMap<>();
+        copy.putAll(Circuitos);
+        return  copy;
+      }
+}
---
+++ src/circuito/SGestCircuitos.java
@@ -0,0 +1,7 @@
+package circuito;
+
+import java.util.List;
+
+public interface SGestCircuitos {
+    public abstract boolean  createCicruito(int voltas, String nomeCirc, String local, float dist, List<Integer> curvas, List<Integer> retas, List<Integer> chicanes, float temperatura, int humidade, float temp_Asf, String estado_climaterico,int drs);
+}
